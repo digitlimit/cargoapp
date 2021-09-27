a57edc5b92d71c619c52ab36839b81fd6dca10f0
Emeka Mbah <frankemeks77@yahoo.com>
2021-09-27 23:23:43 +0100

    add closure

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,13 @@
 pub fn run() {
     let age = get_age(1983, 2021);
     greeting("Hello", "Emeka");
+
     println!("Your age is {}", age);
+
+    // Closure
+    let n3: i32 = 4;
+    let add_nums = |n1: i32, n2: i32| n1 + n2 + n3;
+    println!("{}", add_nums(2, 3));
 }
 
 fn greeting(message: &str, name: &str){
@@ -11,3 +17,4 @@ fn greeting(message: &str, name: &str){
 fn get_age(dob: u16, current_year: u16) -> u16 {
     current_year - dob as u16
 }
+
